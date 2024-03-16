6a5677cbdda598e2dd0469919c2d7a0a0ab08daa
wxp02 <wxp0025@gmail.com>
2024-03-16 00:00:35 -0400

    Getting started

---
+++ index.js
@@ -0,0 +1,10 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// Server setup
+const server = new ApolloServer({});
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log("Server ready at port", 4000);
