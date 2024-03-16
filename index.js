befdb65ed15f59d58a098507df85d13812a76f71
wxp02 <wxp0025@gmail.com>
2024-03-16 00:10:20 -0400

    Define schema and types

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
 // Server setup
-const server = new ApolloServer({});
+const server = new ApolloServer({
+  // typeDefs
+  // resolvers
+});
+
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
 });
