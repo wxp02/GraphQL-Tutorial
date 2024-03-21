681179b87b3f909c1613849a0fbc7cb8be23cc9d
wxp02 <wxp0025@gmail.com>
2024-03-20 23:46:38 -0400

    Mutations

--- index.js
+++ index.js
@@ -46,6 +46,33 @@ const resolvers = {
       return db.reviews.filter((r) => r.author_id === parent.id);
     },
   },
+  Mutation: {
+    addGame(_, args) {
+      let game = {
+        ...args.game,
+        id: Math.floor(Math.random() * 10000).toString(),
+      };
+      db.games.push(game);
+
+      return game;
+    },
+    deleteGame(_, args) {
+      db.games = db.games.filter((g) => g.id !== args.id);
+
+      return db.games;
+    },
+    updateGame(_, args) {
+      db.games = db.games.map((g) => {
+        if (g.id === args.id) {
+          return { ...g, ...args.edits };
+        }
+
+        return g;
+      });
+
+      return db.games.find((g) => g.id === args.id);
+    },
+  },
 };
 
 // Server setup
--- schema.js
+++ schema.js
@@ -1,30 +1,42 @@
-// If there is an ! at end of field, it means the type is required
 export const typeDefs = `#graphql
-    type Game {
-        id: ID!
-        title: String!
-        platform: [String!]!
-        reviews: [Review!]
-    }
-    type Review {
-        id: ID!
-        rating: Int!
-        content: String!
-        game: Game!
-        author: Author!
-    }
-    type Author {
-        id: ID!
-        name: String!
-        verified: Boolean!
-        reviews: [Review!]
-    }
-    type Query {
-        reviews: [Review]
-        review(id: ID!): Review
-        games: [Game]
-        game(id: ID!): Game
-        authors: [Author]
-        author(id: ID!): Author
-    }
+  type Game {
+    id: ID!
+    title: String!
+    platform: [String!]!
+    reviews: [Review!]
+  }
+  type Review {
+    id: ID!
+    rating: Int!
+    content: String!
+    author: Author!
+    game: Game!
+  }
+  type Author {
+    id: ID!
+    name: String!
+    verified: Boolean!
+    reviews: [Review!]
+  }
+  type Query {
+    games: [Game]
+    game(id: ID!): Game
+    reviews: [Review]
+    review(id: ID!): Review
+    authors: [Author]
+    author(id: ID!): Author
+  }
+  type Mutation {
+    addGame(game: AddGameInput!): Game
+    deleteGame(id: ID!): [Game]
+    updateGame(id: ID!, edits: EditGameInput): Game
+  }
+  input AddGameInput {
+    title: String!,
+    platform: [String!]!
+  }
+  input EditGameInput {
+    title: String,
+    platform: [String!]
+  }
 `;
