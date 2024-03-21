b260997bb8259214149c6c8da066553df1b96843
wxp02 <wxp0025@gmail.com>
2024-03-20 23:36:00 -0400

    Related data

--- index.js
+++ index.js
@@ -13,19 +13,37 @@ const resolvers = {
       return db.games;
     },
     game(_, args) {
-      return db.reviews.find((game) => game.id == args.id);
+      return db.games.find((game) => game.id == args.id);
     },
     authors() {
       return db.authors;
     },
     author(_, args) {
-      return db.reviews.find((author) => author.id == args.id);
+      return db.authors.find((author) => author.id == args.id);
     },
     reviews() {
       return db.reviews;
     },
     review(_, args) {
       return db.reviews.find((review) => review.id == args.id);
+    },
+  },
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.game_id === parent.id);
+    },
+  },
+  Review: {
+    author(parent) {
+      return db.authors.find((a) => a.id === parent.author_id);
+    },
+    game(parent) {
+      return db.games.find((g) => g.id === parent.game_id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.author_id === parent.id);
     },
   },
 };
